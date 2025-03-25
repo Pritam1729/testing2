c3d90e3f0a4fecfbb2951c0ac5dd86ce27b738e4
Pritam S Gurav <106548018+Pritam1729@users.noreply.github.com>
2025-03-25 10:28:20 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+if [ $(($1 + $2)) -eq 12 ]; then
+  echo "TEST PASSED"
+  exit 0
+fi
+if [ $(($1 + $2)) -eq 13 ]; then
+  echo "TEST PASSED"
+  exit 0
+fi
 
+echo "SOME THING IS WRON"
+echo "TEST FAILED"
+exit 1
